335954684a6200bca63619e9a03eb84623bd7a3a
Jordan <jordanveness8@gmail.com>
2021-09-23 12:35:33 +0100

    list-new-space form and tests

--- app.rb
+++ app.rb
@@ -6,8 +6,21 @@ class Makersbnb < Sinatra::Base
     register Sinatra::Reloader 
   end
 
+  # configure do 
+  #   enable :sessions
+  # end
+
   get '/' do
     'Welcome!'
   end
-  
+
+  get '/listspace' do
+    erb :new_space
+  end
+
+  post '/newspace' do
+    'Hello'
+  end
+
+  run! if app_file == $0
 end
